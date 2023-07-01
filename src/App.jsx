fbb461659ecdfa220d60ea39e61b79c00059151c
Helio Sales Jr <heliosalesjr@gmail.com>
2023-07-01 19:58:51 -0400

    testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features'
+import Testimonials from './components/Testimonials'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Features />
+      <Testimonials />
     </>
   )
 }
